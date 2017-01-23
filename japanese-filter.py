93e82d7f54b294103bcf38311031e1a37b04a3f6
Adam Holmes <Atomsphere@me.com>
2017-01-23 14:39:35 +0800

    Add files via upload

--- japanese-filter.py
+++ japanese-filter.py
@@ -11,9 +11,9 @@ sys.setdefaultencoding('utf-8')
 def aozora_to_html_ruby(text):
     '''aozorabunko format ruby to html
     e.g,
-    難読|漢字《かんじ》でも読《よ》めますよ。
+    難読｜漢字《かんじ》でも読《よ》めますよ。
     to
-    難読|<ruby>漢字<rt>かんじ</rt></ruby>でも<ruby>読<rt>よ</rt></ruby>めますよ'''
+    難読<ruby>漢字<rt>かんじ</rt></ruby>でも<ruby>読<rt>よ</rt></ruby>めますよ。'''
     s = text.decode('utf-8')
     pattern = ur'｜?([\u4e00-\u9fff]+?)《([\u3040-\u309f]+?)》'
     repl = ur"<ruby>\1<rt>\2</rt></ruby>"
@@ -22,7 +22,7 @@ def aozora_to_html_ruby(text):
 
 def for_japanese(key, value, format, meta):
     if key == 'Str':
-        return RawInline(u'html',aozora_to_html_ruby(value))
+        return RawInline(u'html', aozora_to_html_ruby(value))
 
 if __name__ == "__main__":
     toJSONFilter(for_japanese)
